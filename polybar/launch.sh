129f90875190c427926e089c6a8eb6ac4a485634
Josue H. V <46503611+HeNeos@users.noreply.github.com>
2022-12-23 02:00:34 -0500

    Add files via upload

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,8 +5,31 @@
 # Terminate already running bar instances
 killall -q polybar
 
-# Wait until the processes have been shut down
+## Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar -c ~/.config/polybar/config.ini main &
+## Launch
+
+## Left bar
+polybar log -c ~/.config/polybar/current.ini &
+polybar secondary -c ~/.config/polybar/current.ini &
+polybar third -c ~/.config/polybar/current.ini &
+
+## Right bar
+polybar top -c ~/.config/polybar/current.ini &
+polybar primary -c ~/.config/polybar/current.ini &
+
+## Center bar
+polybar primary -c ~/.config/polybar/workspace.ini &
+
+## Left bar
+polybar log2 -c ~/.config/polybar/current.ini &
+polybar secondary2 -c ~/.config/polybar/current.ini &
+polybar third2 -c ~/.config/polybar/current.ini &
+
+## Right bar
+polybar top2 -c ~/.config/polybar/current.ini &
+polybar primary2 -c ~/.config/polybar/current.ini &
+
+## Center bar
+polybar primary2 -c ~/.config/polybar/workspace.ini &

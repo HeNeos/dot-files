f9ed3d8180a62e5395c7a32b6a0bcbaf218bd3c5
HeNeos <heneos3.14@gmail.com>
2024-03-24 21:19:21 +0100

    Read from local yasb

--- yasb/weather.py
+++ yasb/weather.py
@@ -1,24 +1,33 @@
 import requests
 import json
 
+from pathlib import Path
+
 def get_location():
     url = "https://ipinfo.io"
     try:
         response = requests.get(url)
         response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
         data = response.json()
-        location = data["loc"]
-        location = location.split(',')
-        return location
+        return {
+            "ip": data["ip"],
+            "city": data["city"],
+            "region": data["region"],
+            "country": data["country"],
+            "loc": data["loc"].split(",")
+        }
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
+        file = open("weather.json")
+        json_data = json.load(file)
+        return json_data
 
 def get_weather_data(api_key):
     location = get_location()
     url = "https://api.openweathermap.org/data/3.0/onecall"
     params = {
-        "lat": float(location[0]),
-        "lon": float(location[1]),
+        "lat": float(location["loc"][0]),
+        "lon": float(location["loc"][1]),
         "units": "metric",
         "appid": api_key
     }
@@ -26,9 +35,17 @@ def get_weather_data(api_key):
     try:
         response = requests.get(url, params=params)
         response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
+        response_json = response.json()
+        
+        response_json |= location
+
+        # Save it in a JSON file
+        file_path = Path.home() / ".yasb" / "weather.json"
+        with open(file_path, "w") as f:
+            json.dump(response_json, f, indent=2)
 
         # Print the JSON response
-        print(json.dumps(response.json(), indent=2))
+        print(json.dumps(response_json, indent=2))
 
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
@@ -36,4 +53,3 @@ def get_weather_data(api_key):
 if __name__ == "__main__":
     api_key = "API_KEY"
     get_weather_data(api_key)
-

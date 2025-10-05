1a86644311b1b7f72385e28ac9cc1b37660368ca
HeNeos <heneos3.14@gmail.com>
2025-10-04 23:49:56 -0500

    Fix scripts

--- yasb/ip_info.py
+++ yasb/ip_info.py
@@ -3,30 +3,40 @@ import json
 
 from pathlib import Path
 
+weather_json_path = Path.home() / ".config" / ".yasb" / "weather.json"
+
+
 def get_location():
     data_keys = ["ip", "city", "region", "country", "loc"]
     url = "https://ipinfo.io"
+
+    try:
+        with open(weather_json_path, "r") as f:
+            json_data = json.load(f)
+    except:
+        json_data = {}
+
     try:
         response = requests.get(url)
-        response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
-        data = response.json()
-        ip_json = {key: data[key] for key in data_keys}
-        ip_json["loc"] = data["loc"].split(",")
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching ip: {e}")
+        match response.status_code:
+            case 200:
+                data = response.json()
+                ip_json = {key: data[key] for key in data_keys}
+                ip_json["loc"] = data["loc"].split(",")
+            case _:
+                raise Exception
+    except Exception:
         ip_json = dict()
-    file_path = Path.home() / ".yasb" / "weather.json"
-    file = open(file_path)
-    json_data = json.load(file)
+
     json_data |= ip_json
 
-    # Save it in a JSON file
-    with open(file_path, "w") as f:
+    with open(weather_json_path, "w") as f:
         json.dump(json_data, f, indent=2)
 
     output = {key: json_data[key] for key in data_keys}
     print(json.dumps(output, indent=2))
     return output
+
 
 if __name__ == "__main__":
     get_location()
--- yasb/weather.py
+++ yasb/weather.py
@@ -3,42 +3,47 @@ import json
 
 from pathlib import Path
 
-def get_location():
+weather_json_path = Path.home() / ".config" / ".yasb" / "weather.json"
+
+
+def get_location() -> dict:
     data_keys = ["ip", "city", "region", "country", "loc"]
-    file_path = Path.home() / ".yasb" / "weather.json"
-    file = open(file_path)
+    file = open(weather_json_path)
     json_data = json.load(file)
-    
+
     return {key: json_data[key] for key in data_keys}
 
-def get_weather_data(api_key):
+
+def get_weather_data(api_key: str) -> None:
     location = get_location()
     url = "https://api.openweathermap.org/data/3.0/onecall"
     params = {
         "lat": float(location["loc"][0]),
         "lon": float(location["loc"][1]),
         "units": "metric",
-        "appid": api_key
+        "appid": api_key,
     }
 
     try:
         response = requests.get(url, params=params)
-        response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
-        response_json = response.json()
-        
-        response_json |= location
+        match response.status_code:
+            case 200:
+                response_json = response.json()
+                response_json |= location
+            case _:
+                with open(weather_json_path, "r") as f:
+                    response_json = json.load(f)
+    except Exception:
+        with open(weather_json_path, "r") as f:
+            response_json = json.load(f)
+        # print(f"Error fetching weather: {e}, using old cache")
 
-        # Save it in a JSON file
-        file_path = Path.home() / ".yasb" / "weather.json"
-        with open(file_path, "w") as f:
-            json.dump(response_json, f, indent=2)
+    with open(weather_json_path, "w") as f:
+        json.dump(response_json, f, indent=2)
 
-        # Print the JSON response
-        print(json.dumps(response_json, indent=2))
+    print(json.dumps(response_json, indent=2))
 
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching weather: {e}")
 
 if __name__ == "__main__":
-    api_key = "API_KEY"
+    api_key = "6e34c8cbd8713a6a9528e9ff880c75eb"
     get_weather_data(api_key)

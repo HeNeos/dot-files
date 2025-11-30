af5c6149af02048fb1562218964529fe8cfd896e
HeNeos <heneos3.14@gmail.com>
2025-11-30 21:10:57 +0000

    Update clangd config

--- neovim/nvim/lua/config/lspconfig.lua
+++ neovim/nvim/lua/config/lspconfig.lua
@@ -1,16 +1,23 @@
-local lspconfig = require("lspconfig")
-
 vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
 
-local on_attach = function(client)
-    require('completion').on_attach(client)
+local on_attach = function(client, bufnr)
+  local opts = { noremap = true, silent = true, buffer = bufnr }
 end
 
-local servers = { "html", "cssls", "denols", "clangd", "gopls", "pyright" }
+local servers = { "html", "cssls", "denols", "gopls" }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {}
+  vim.lsp.config[lsp] = {on_attach = on_attach,}
 end
+
+vim.lsp.enable("clangd")
+vim.lsp.config.clangd = {
+  init_options = {
+    usePlaceholders = true,
+    completeUnimported = true,
+    clangdFileStatus = true,
+  }
+}
 
 -- require("lspconfig").rust_analyzer.setup({
 --     on_attach = on_attach,
@@ -34,8 +41,8 @@ end
 --     }
 -- })
 
-require("lspconfig").bacon_ls.setup({
-    init_options = {
+vim.lsp.config("bacon_ls", {
+     init_options = {
         updateOnSave = true,
         updateOnSaveWaitMillis = 1000,
         updateOnChange = true,
@@ -44,6 +51,17 @@ require("lspconfig").bacon_ls.setup({
     }
 })
 
+-- require("lspconfig").bacon_ls.setup({
+--     init_options = {
+--         updateOnSave = true,
+--         updateOnSaveWaitMillis = 1000,
+--         updateOnChange = true,
+--         validateBaconPreferences = true,
+--         runBaconInBackground = true,
+--     }
+-- })
+
+vim.lsp.enable("ruff")
 -- vim.lsp.config('ruff', {
 --   init_options = {
 --     settings = {
@@ -52,7 +70,36 @@ require("lspconfig").bacon_ls.setup({
 --   }
 -- })
 
-vim.lsp.enable('ruff')
+vim.lsp.enable("basedpyright")
+vim.lsp.config.basedpyright = {
+  root_dir = vim.fs.root(0, {
+    "pyproject.toml",
+    "setup.py",
+    "setup.cfg",
+    "requirements.txt",
+    "Pipfile",
+    ".git",
+  }),
+  on_attach = on_attach,
+  settings = {
+    basedpyright = {
+      analysis = {
+        autoSearchPaths = true,
+        diagnosticMode = "openFilesOnly",
+        useLibraryCodeForTypes = true,
+      },
+    },
+    pyright = {
+      disableOrganizeImports = true,
+    },
+    python = {
+      analysis = {
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
+      },
+    },
+  },
+}
 
 
 -- vim.lsp.config("basedpyright", {
@@ -71,31 +118,35 @@ vim.lsp.enable('ruff')
 --   },
 -- })
 
-lspconfig.basedpyright.setup({
-    root_dir = lspconfig.util.root_pattern(
-        "pyproject.toml",
-        "setup.py",
-        "setup.cfg",
-        "requirements.txt",
-        "Pipfile",
-        ".git"
-    ),
-    settings = {
-        basedpyright = {
-            analysis = {
-                autoSearchPaths = true,
-                diagnosticMode = "openFilesOnly",
-                useLibraryCodeForTypes = true,
-            },
-        },
-        pyright = {
-            disableOrganizeImports = true,
-        },
-        python = {
-            analysis = {
-                autoSearchPaths = true,
-                useLibraryCodeForTypes = true,
-            },
-        },
-    },
-})
+-- lspconfig.basedpyright.setup({
+--     root_dir = lspconfig.util.root_pattern(
+--         "pyproject.toml",
+--         "setup.py",
+--         "setup.cfg",
+--         "requirements.txt",
+--         "Pipfile",
+--         ".git"
+--     ),
+--     settings = {
+--         basedpyright = {
+--             analysis = {
+--                 autoSearchPaths = true,
+--                 diagnosticMode = "openFilesOnly",
+--                 useLibraryCodeForTypes = true,
+--             },
+--         },
+--         pyright = {
+--             disableOrganizeImports = true,
+--         },
+--         python = {
+--             analysis = {
+--                 autoSearchPaths = true,
+--                 useLibraryCodeForTypes = true,
+--             },
+--         },
+--     },
+-- })
+
+for name, _ in pairs(vim.lsp.config) do
+  vim.lsp.enable(name)
+end
